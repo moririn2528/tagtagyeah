eee26c50d2363d932554b42a552aea32fbe8a660
moririn2528 <strangenoise1@gmail.com>
2021-08-24 03:44:38 +0900

    modify to deploy

--- test.sql
+++ test.sql
@@ -1,5 +1,6 @@
 use tagtagyeah; -- local
-use heroku_acb493180552729; -- heroku
+use heroku_acb493180552729; -- heroku ClearDB
+use eij8pzwnprvffh1t; -- heroku JawsDB
 drop table tag_table;
 create table tag_table(
 	id int auto_increment not null primary key,
@@ -8,7 +9,7 @@ create table tag_table(
 	created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-insert into tag_table (user_id,name) values (0,"test1");
+insert into tag_table (user_id,name) values (0,"tag_testW");
 select * from tag_table;
 delete from tag_table where user_id = 0;
 INSERT INTO tag_table (user_id,name) SELECT (0,"testA")
